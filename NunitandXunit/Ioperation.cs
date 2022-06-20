2bdc02f52232889e810c2ecc442155a457861e8e
ANIRUDH SINGH <anirudh8400@gmail.com>
2022-06-20 07:23:22 +0530

    first commit

---
+++ NunitandXunit/Ioperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitandXunit
+{
+    public interface Ioperation
+    {
+        double add(double a, double b);
+        double sub(double a, double b);
+        double mul(double a, double b);
+        double div(double a, double b);
+
+    }
+}
---
+++ NunitandXunit/Operation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitandXunit
+{
+    internal class Operation
+    {
+        public double Additions(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Substraction(double a, double b)
+        {
+            return a - b;
+
+        }
+
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
+
